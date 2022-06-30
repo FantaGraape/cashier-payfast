b016f88865a31b519510d1a7b024a83c41cedfb2
Timothy <tim.101.ellis@gmail.com>
2022-06-30 14:55:37 +0200

    Orders Test innit

---
+++ tests/Feature/OrdersTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Tests\Feature;
+
+use Carbon\Carbon;
+use EllisSystems\Payfast\Cashier;
+use EllisSystems\Payfast\Order;
+use LogicException;
+
+class OrdersTest extends FeatureTestCase
+{
+    //
+}
